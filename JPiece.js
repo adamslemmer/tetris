0b2636cdf6de684a5417feb31404d2f36bec6d71
Adam Slemmer <adam@slemmer.com>
2024-02-12 16:56:08 -0800

    tetris game

---
+++ JPiece.js
@@ -0,0 +1,21 @@
+var JPiecerot = function(pos, piece){
+    var divlist = [];
+    for (i = 0; i < counter; i++){
+        if (document.getElementById(i).className == piece){
+            divlist.push(i);
+        }
+    }
+
+    var rots = [
+        [[-20, 20], [0, 0], [20, -20], [40, 0]], 
+        [[20, 20], [0, 0], [-20, -20], [0, -40]], 
+        [[20, -20], [0, 0], [-20, 20], [-40, 0]], 
+        [[-20, -20], [0, 0], [20, 20], [0, 40]]
+    ]
+
+    for (i = 0; i < divlist.length; i++){
+        var thisdiv = document.getElementById(divlist[i]);
+        thisdiv.style.left = thisdiv.offsetLeft + rots[pos][i][0] + "px";
+        thisdiv.style.top = thisdiv.offsetTop + rots[pos][i][1] + "px";
+    }
+}
